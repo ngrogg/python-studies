916055c1082488bbecfb0d457a1c4589f9e4296b
Nicholas Grogg <oddiusoddone@gmail.com>
2023-01-15 07:20:30 +0000

    Added loops to concepts

---
+++ concepts/loops/loops.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Import statements 
+import sys
+
+# Loops 
+# By Nicholas Grogg 
+
+## Function to run program
+def runProgram():
+    ### Array of numbers 
+    numList = [1,2,3,4]
+
+    ### Iterator object for list
+    listIt  = iter(numList)
+
+    ### Loop through list with iterator, add whitespace 
+    print("Using for loop")
+    for i in listIt:
+        print(i, end=" ")
+
+    ### Print newline 
+    print("\n")
+
+    ### Loop using while loop
+    print("Using while loop")
+    count   = 0
+    while (count < 4):
+        print(numList[count])
+        count += 1
+
+## Run Program
+runProgram()
